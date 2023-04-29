546cc631a5580e6bdaf4d5540e3f942f0a83b4dc
log-msmir <msmir@inbox.ru>
2023-04-29 12:52:56 +0300

    Full updated setup.cfg

--- ignoreme.py
+++
@@ -1,4 +0,0 @@
-flake8 = ['__pycache__', migrations, '__init__', '.github', 'templates',
-           'venv', '.dockerignore', '.gitingore', 'docker-compose.yaml',
-           'docker-entrypoint.sh', 'Dockerfile', 'dump.json', 'host.conf',
-           'openapi-schema.yml', 'requirements.txt', 'settings.py']
